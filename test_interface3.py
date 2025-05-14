6db53631f223760573802c51cf1c9710b237abf0
Amn Amine <105323588+amnamine@users.noreply.github.com>
2025-05-14 11:36:19 +0100

    spanish

--- test_interface3.py
+++ test_interface3.py
@@ -124,7 +124,7 @@ class TranslationApp:
         self.source_lang_label = tk.Label(lang_frame, text="From:", bg=self.style['bg'])
         self.source_lang_label.pack(side=tk.LEFT, padx=5)
         self.source_lang_combo = ttk.Combobox(lang_frame, textvariable=self.source_lang_var, 
-                                            values=['Arabic', 'English', 'French', 'Latin'])
+                                            values=['Arabic', 'English', 'French', 'Latin', 'Spanish'])
         self.source_lang_combo.pack(side=tk.LEFT, padx=5)
 
         # Target language selection
@@ -132,7 +132,7 @@ class TranslationApp:
         self.target_lang_label = tk.Label(lang_frame, text="To:", bg=self.style['bg'])
         self.target_lang_label.pack(side=tk.LEFT, padx=5)
         self.target_lang_combo = ttk.Combobox(lang_frame, textvariable=self.target_lang_var, 
-                                            values=['Arabic', 'English', 'French', 'Latin'])
+                                            values=['Arabic', 'English', 'French', 'Latin', 'Spanish'])
         self.target_lang_combo.pack(side=tk.LEFT, padx=5)
 
         for combo in [self.source_lang_combo, self.target_lang_combo]:
@@ -296,7 +296,8 @@ class TranslationApp:
                 'Arabic': 'ar',
                 'English': 'en',
                 'French': 'fr',
-                'Latin': 'la'
+                'Latin': 'la',
+                'Spanish': 'es'
             }
             
             self.status_bar['text'] = "Translating..."
@@ -383,7 +384,8 @@ class TranslationApp:
                     'Arabic': 'ar',
                     'English': 'en',
                     'French': 'fr',
-                    'Latin': 'la'
+                    'Latin': 'la',
+                    'Spanish': 'es'
                 }
                 
                 lang = lang_map[self.target_lang_var.get()]
@@ -416,7 +418,8 @@ class TranslationApp:
                 'Arabic': 'ar',
                 'English': 'en',
                 'French': 'fr',
-                'Latin': 'la'  # Note: Latin might not be supported by gTTS
+                'Latin': 'la',
+                'Spanish': 'es'
             }
             
             lang = lang_map[self.target_lang_var.get()]
